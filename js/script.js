50c567db211f1c5db47d961403d957cd15ed5842
chilian_s <sierrachilian@gmail.com>
2024-01-14 17:00:09 -0500

    Added global variables and event listener

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
+const playerGuessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const input = document.querySelector("input");
+const inProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const span = document.querySelector("span");
+const message = document.querySelector(".message")
+const hiddenButton = document.querySelector(".play-again")
+
+const word = "magnolia";
+
+
+const letterSymbol = function (word) {
+    const symbols = [];
+    for (const letter of word) {
+        console.log(letter);
+        symbols.push("●");
+    }
+
+    inProgress.innerText = symbols.join("");
+
+    };
+
+    letterSymbol(word);
+
+    guessButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        
+        const guessInput = input.value;
+        console.log(guessInput);
+        input.value = "";
+    });
+
 
 
